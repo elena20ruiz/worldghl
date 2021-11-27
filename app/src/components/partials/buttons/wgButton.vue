84b1d5568226a182aa3ddb58401569624b74d1b7
Elena <elena.ruiz.bdn@gmail.com>
2021-11-27 23:35:58 +0100

    :sparkles: Add WgButton filled & outlined + primary & secondary. Update MainPage setting the desired layout

--- app/src/components/partials/buttons/wgButton.vue
+++ app/src/components/partials/buttons/wgButton.vue
@@ -1,6 +1,8 @@
 <template>
-  <button class="wg-btn">
-    {{ child }}
+  <button :class="['wg-btn', `wg-btn--${type}`, `wg-btn--${color}`]" @click="onClick">
+    <label>
+      <slot />
+    </label>
   </button>
 </template>
 
@@ -8,42 +10,95 @@
   export default {
     name: 'WgButton',
     props: {
-      text: String
+      type: {
+        type: String,
+        default: 'raised'
+      },
+      color: {
+        type: String,
+        default: 'primary'
+      }
+    },
+    emits: ['click'],
+    methods: {
+      onClick () {
+        this.$emit('click')
+      }
     }
   }
 </script>
 <style scoped lang="scss">
   .wg-btn {
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 18px;
-    text-transform: uppercase;
-    color: $wg-neutral-dark;
-    background-color: $wg-primary-color;
-    transition: all .2s ease-in-out;
     display: inline-block;
-    height: 36px;
-    padding: 0 26px;
-    margin-top: 6px;
-    margin-bottom: 6px;
-    border: none;
-    border-radius: 2px;
-    cursor: pointer;
-    touch-action: manipulation;
-    background-image: none;
-    text-align: center;
-    line-height: 36px;
-    vertical-align: middle;
-    white-space: nowrap;
-    user-select: none;
-    font-size: 14px;
-    letter-spacing: .03em;
-    position: relative;
     overflow: hidden;
 
-    &:active, &:focus,&:hover {
-      color: rgba(0,0,0,.87);
-      background-color: #fff
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+    letter-spacing: .03em;
+    line-height: 36px;
+    vertical-align: middle;
+    text-transform: uppercase;
+    text-align: center;
+    white-space: nowrap;
+    transition: all .2s ease-in-out;
+
+    border: none;
+    border-radius: 4px;
+
+    cursor: pointer;
+    touch-action: manipulation;
+    user-select: none;
+
+    &--raised {
+      padding: 0 16px;
+      box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12);
+
+      &.wg-btn {
+        &--primary {
+          color: $wg-on-primary-color;
+          background-color: $wg-primary-color;
+
+          &:active,&:hover {
+            color: $wg-primary-container-color;
+            background-color: $wg-on-primary-container-color;
+          }
+        }
+
+        &--secondary {
+          color: $wg-on-secondary-color;
+          background-color: $wg-secondary-color;
+          &:active, &:focus,&:hover {
+            color: $wg-secondary-container-color;
+            background-color: $wg-on-secondary-container-color;
+          }
+        }
+      }
+    }
+
+    &--outlined {
+      background: transparent;
+      &.wg-btn {
+        &--primary {
+          border: 1px solid $wg-on-primary-color;
+          color: $wg-primary-container-color;
+
+          &:active,&:hover {
+            border: 1px solid $wg-on-primary-container-color;
+            color: $wg-primary-container-color;
+          }
+        }
+
+        &--secondary {
+          border: 1px solid $wg-on-secondary-color;
+          color: $wg-secondary-container-color;
+
+          &:active,&:hover {
+            border: 1px solid $wg-on-secondary-container-color;
+            color: $wg-secondary-color;
+          }
+        }
+      }
     }
   }
 </style>
--- app/src/views/MainPage.vue
+++ app/src/views/MainPage.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="main-page" class="wg-container">
+  <div id="main-page" class="wg-container wg-main-page">
     <div class="wg-row">
-      <div class="wg-col wg-col--50">
-        TITLE
+      <div class="wg-col wg-col--50 wg-background--primary">
+        <img src="../assets/img/logo.png" />
       </div>
-      <div class="wg-col wg-col--50">
-        MENU
-        <wg-button> text </wg-button>
+      <div class="wg-col wg-col--50 wg-main-page__menu">
+        <div class="wg-main-page__group-buttons">
+          <wg-button color="primary"> text </wg-button>
+          <wg-button color="primary"> text </wg-button>
+        </div>
       </div>
     </div>
   </div>
@@ -23,4 +25,20 @@
   #main-page {
     height: 100vh;
   }
+
+  .wg-main-page {
+    &__menu {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: inherit;
+    }
+    &__group-buttons {
+      display: flex;
+      flex-direction: column;
+      width: 30em;
+      gap: 1em;
+    }
+  }
+
 </style>

b71273a8033a3c5ebeb531a03fb3ab90a93e49a8
Elena <elena.ruiz.bdn@gmail.com>
2022-01-16 16:47:05 +0100

    :lipstick: Change logo from main page and center it

--- app/src/layouts/Brand/MainBrand.vue
+++ app/src/layouts/Brand/MainBrand.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wg-main-brand">
     <div class="wg-main-brand__img">
-      <img src="../../assets/img/logo.png" />
+      <img src="../../assets/img/logo3.png" />
     </div>
   </div>
 </template>
@@ -10,3 +10,17 @@
     name: 'MainBrand'
   }
 </script>
+<style lang="scss">
+  .wg-main-brand {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    &__img {
+      img {
+        width: 80%;
+        max-width: 400px;
+      }
+    }
+  }
+</style>

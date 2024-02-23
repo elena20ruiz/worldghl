a2ad48d29c9b8c246ac463e8f5784793d564ad2d
mariaabas <maria.abasprades@streamsets.com>
2024-02-24 00:48:58 +0100

    Show only the play button on the main game screen.

---
+++ app/src/layouts/Menu/GamePlay.vue
@@ -0,0 +1,38 @@
+<template>
+     <wg-button class="wg-game-menu__play" @click="onStartGame">PLAY</wg-button> 
+</template>
+<script>
+import WgButton from '../../components/partials/buttons/wgButton.vue'
+  export default {
+    name: 'GamePlay',
+    components: { WgButton },
+    data () {
+      return {
+        categories: [{
+          id: 'POPULATION_CON'
+        }]
+      }
+    },
+    computed: {},
+    methods: {
+      onStartGame () {
+        this.$router.push({ name: 'GamePage', params: { game: this.categories[0].id } })
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .wg-game-menu {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+
+    &__play {
+      margin-top: 20px;
+      width: 14rem;
+      height: 4rem;
+      border-radius: 8px !important;
+      font-size: 80% !important;
+    }
+  }
+</style>
